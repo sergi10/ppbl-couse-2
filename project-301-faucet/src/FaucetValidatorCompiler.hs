74d15f0129aa8ef64117db6e2a3c45e68cd07ca1
gimbalabs <gimbalabs@jamess-MacBook-Pro.local>
2022-08-11 10:13:44 -0400

    Working on Project 301

--- project-301-faucet/src/FaucetValidatorCompiler.hs
+++ project-301-faucet/src/FaucetValidatorCompiler.hs
@@ -9,11 +9,17 @@ import Codec.Serialise (serialise)
 import qualified Data.ByteString.Lazy as LBS
 import qualified Data.ByteString.Short as SBS
 import qualified Ledger
-import qualified FaucetValidatorScript
+import qualified FaucetValidatorScript (validator, FaucetParams (..))
 
 
 writeValidator :: FilePath -> Ledger.Validator -> IO (Either (FileError ()) ())
 writeValidator file = writeFileTextEnvelope @(PlutusScript PlutusScriptV1) file Nothing . PlutusScriptSerialised . SBS.toShort . LBS.toStrict . serialise . Ledger.unValidatorScript
 
-writeMyFirstValidatorScript :: IO (Either (FileError ()) ())
-writeMyFirstValidatorScript = writeValidator "output/ppbl-faucet.plutus" FaucetValidatorScript.validator
+writeFaucetScript :: IO (Either (FileError ()) ())
+writeFaucetScript = writeValidator "output/ppbl-faucet.plutus" $ FaucetValidatorScript.validator $ FaucetValidatorScript.FaucetParams
+    {
+      FaucetValidatorScript.accessTokenSymbol     = "1309921891e459c7e9acb338d5dae18f98d1c2f55c1852cd5cf341f9"
+    , FaucetValidatorScript.accessTokenName       = "PPBLSummer2022"
+    , FaucetValidatorScript.faucetTokenSymbol     = "6c57132fde399c9ea6e462c4214d164984891087922b6fa2472b175b"
+    , FaucetValidatorScript.faucetTokenName       = "tpblTestGimbal"
+    }
--- project-301-faucet/src/FaucetValidatorScript.hs
+++ project-301-faucet/src/FaucetValidatorScript.hs
@@ -12,41 +12,71 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 
 
-module FaucetValidatorScript where
+module FaucetValidatorScript 
+  ( FaucetParams (..)
+  , validator
+  ) where
 
+
+import              Data.Aeson                (ToJSON, FromJSON)
+import              GHC.Generics              (Generic)
+import              Schema                    (ToSchema)
 import              Ledger              hiding (singleton)
+import              Ledger.Address
 import              Ledger.Typed.Scripts
 import              Ledger.Value        as Value
 import              Ledger.Ada
 import qualified    PlutusTx
 import              PlutusTx.Prelude    hiding (Semigroup (..), unless)
 import              Prelude             (Show (..))
-import Ledger.Address
+import qualified    Prelude                   as Pr
+
 
 -- We're going to need some parameters - great chance to apply this concept!
 
--- The Redeemer could be irrelevant.
--- The Datum could be used just to show how it works.
+-- The Redeemer could be irrelevant, or it could have one option: Withdraw
+-- The Datum could be used just to show how it works. Could include some info about token, like withdrawal amount?
 -- The context matters: we want to see that the PPBLSummer2022 token is in the transaction inputs and outputs.
 
+-- Usage:
+-- One utxo at each contract.
+-- Take that utxo as input
+-- Create a new one with the "change" as output
+
+data FaucetParams = FaucetParams
+  { accessTokenSymbol   :: !CurrencySymbol
+  , accessTokenName     :: !TokenName
+  , faucetTokenSymbol   :: !CurrencySymbol
+  , faucetTokenName     :: !TokenName
+  } deriving (Pr.Eq, Pr.Ord, Show, Generic, ToJSON, FromJSON, ToSchema)
+
+PlutusTx.makeLift ''FaucetParams
+
 {-# INLINEABLE faucetValidator #-}
-faucetValidator :: Integer -> Integer -> ScriptContext -> Bool
-faucetValidator _ _ ctx =   traceIfFalse "Input needs PPBLSummer2022 token" inputHasToken &&
-                            traceIfFalse "PPBLSummer2022 token must return to sender" outputHasToken &&
-                            traceIfFalse "Withdrawal limit exceeded" checkWithdrawalLimit &&
-                            traceIfFalse "Do we need to check datum" checkDatumIsOk
+faucetValidator :: FaucetParams -> Integer -> Integer -> ScriptContext -> Bool
+faucetValidator faucet _ _ ctx =   traceIfFalse "Input needs PPBLSummer2022 token"    inputHasAccessToken &&
+                            traceIfFalse "PPBLSummer2022 token must return to sender" outputHasAccessToken &&
+                            traceIfFalse "Faucet token must be distributed to sender" outputHasFaucetToken &&
+                            traceIfFalse "Must return remaining tokens to contract"   contractGetsRemainingTokens &&
+                            traceIfFalse "Do we need to check datum"                  checkDatumIsOk
   where
     info :: TxInfo
     info = scriptContextTxInfo ctx
 
-    inputHasToken :: Bool
-    inputHasToken = True
+    allTokens :: [CurrencySymbol]
+    allTokens = symbols $ valueSpent info
 
-    outputHasToken :: Bool
-    outputHasToken = True
+    inputHasAccessToken :: Bool
+    inputHasAccessToken = (accessTokenSymbol faucet) `elem` allTokens
 
-    checkWithdrawalLimit :: Bool
-    checkWithdrawalLimit = True
+    outputHasAccessToken :: Bool
+    outputHasAccessToken = True
+
+    outputHasFaucetToken :: Bool
+    outputHasFaucetToken = True
+
+    contractGetsRemainingTokens :: Bool
+    contractGetsRemainingTokens = True
 
     checkDatumIsOk :: Bool
     checkDatumIsOk = True
@@ -57,14 +87,14 @@ instance ValidatorTypes FaucetTypes where
     type DatumType FaucetTypes = Integer
     type RedeemerType FaucetTypes = Integer
 
-typedValidator :: TypedValidator FaucetTypes
-typedValidator =
+typedValidator :: FaucetParams -> TypedValidator FaucetTypes
+typedValidator faucet =
   mkTypedValidator @FaucetTypes
-    $$(PlutusTx.compile [||faucetValidator||])
+    ($$(PlutusTx.compile [||faucetValidator||]) `PlutusTx.applyCode` PlutusTx.liftCode faucet)
     $$(PlutusTx.compile [||wrap||])
   where
     wrap = wrapValidator @Integer @Integer
 
 
-validator :: Validator
-validator = validatorScript typedValidator
+validator :: FaucetParams -> Validator
+validator = validatorScript . typedValidator
