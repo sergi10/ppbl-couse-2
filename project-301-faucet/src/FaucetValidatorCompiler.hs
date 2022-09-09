53285872f85025d2e839cddd5e2bd0d7eb75edec
jamesdunseith <jdunseith@gimbalabs.io>
2022-09-09 15:16:41 -0400

    Update FaucetValidatorCompiler to use FaucetValidatorScriptWithPkh

--- project-301-faucet/src/FaucetValidatorCompiler.hs
+++ project-301-faucet/src/FaucetValidatorCompiler.hs
@@ -16,14 +16,14 @@ writeValidator :: FilePath -> Ledger.Validator -> IO (Either (FileError ()) ())
 writeValidator file = writeFileTextEnvelope @(PlutusScript PlutusScriptV1) file Nothing . PlutusScriptSerialised . SBS.toShort . LBS.toStrict . serialise . Ledger.unValidatorScript
 
 -- This is a comment in Haskell
--- 1. Change the name of the output file to ppbl-faucet-yourTokenName.plutus
+-- 1. Change the name of the output file to ppbl-faucet-preprod-yourTokenName.plutus
 -- 2. Change the faucetTokenSymbol and faucetTokenName to match the token that you want to distribute
 -- 3. Change the withdrawalAmount to whatever amount you want people to able to claim in one transaction
 -- 4. Compile your script
 -- 5. Recommended: take a screenshot of this file so that you can remember the parameters you used
 
 writeFaucetScript :: IO (Either (FileError ()) ())
-writeFaucetScript = writeValidator "output/ppbl-faucet-preprod-minimum.plutus" $ FaucetValidatorScriptWithPkh.validator $ FaucetValidatorScriptWithPkh.FaucetParams
+writeFaucetScript = writeValidator "output/ppbl-faucet-preprod-<YOUR TOKEN NAME>.plutus" $ FaucetValidatorScriptWithPkh.validator $ FaucetValidatorScriptWithPkh.FaucetParams
     {
       FaucetValidatorScriptWithPkh.accessTokenSymbol     = "748ee66265a1853c6f068f86622e36b0dda8edfa69c689a7dd232c60"
     , FaucetValidatorScriptWithPkh.accessTokenName       = "PPBLSummer2022"
